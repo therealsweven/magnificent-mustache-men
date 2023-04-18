c03a74044a61927de2c1a98c8a0c92f03022e854
therealsweven <robssimpson11@gmail.com>
2023-04-17 21:11:45 -0600

    Co-authored-by: joshua-wade7 <joshua-wade7@users.noreply.github.com>

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,11 +1,29 @@
 const db = require("../config/connection");
-const { User } = require("../models");
+const {
+  User,
+  Job,
+  Post,
+  Company,
+  Comment,
+  EducationalInstitution,
+  Group,
+  Reaction,
+  Skill,
+} = require("../models");
 const userSeeds = require("./userSeeds.json");
+const companySeeds = require("./companySeeds.json");
+const universitySeeds = require("./universities.json");
 
 db.once("open", async () => {
   try {
     await User.deleteMany({});
     await User.create(userSeeds);
+
+    await Company.deleteMany({});
+    await Company.create(companySeeds);
+
+    await EducationalInstitution.deleteMany({});
+    await EducationalInstitution.create(universitySeeds);
 
     console.log("all done!");
     process.exit(0);
