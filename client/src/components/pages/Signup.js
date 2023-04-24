e40c9cabf1080f86559e12a9c33fd23b3eba01db
Michael Mount <mount.photos@gmail.com>
2023-04-24 10:42:39 -0600

    fixing lorem ipsium

--- client/src/components/pages/Signup.js
+++ client/src/components/pages/Signup.js
@@ -11,7 +11,7 @@ export default function Signup() {
   <div className="hero-content flex-col lg:flex-row-reverse">
     <div className="text-center lg:text-left">
       <h1 className="text-5xl font-bold">Sign Up</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <p className="py-6">Welcome to an Exciting New PlatForm built just For Tech Developers Just Like You! Welcom to your new Networking Space!</p>
     </div>
     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
       <div className="card-body">
