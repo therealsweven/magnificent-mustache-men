6390720906fee242021e2ed64c9e7e5aa6946a4a
Michael Mount <mount.photos@gmail.com>
2023-04-23 18:59:24 -0600

    reworked DashBoard

--- client/src/components/pages/UserDashBoard.js
+++ client/src/components/pages/UserDashBoard.js
@@ -1,18 +1,18 @@
 import { useQuery } from "@apollo/client";
-import { QUERY_JOBS, QUERY_FEED } from "../../utils/queries";
+import { QUERY_JOBS, QUERY_FEED, QUERY_ME } from "../../utils/queries";
 import PostForm from "./forms/PostForm";
 
 export default function UserDashboard() {
   const { loading: jobLoading, data: jobData } = useQuery(QUERY_JOBS);
   const { loading: feedLoading, data: feedData } = useQuery(QUERY_FEED);
+  const { loading, data } = useQuery(QUERY_ME);
 
   // const { load, feedData } = useQuery(QUERY_FEED);
 
   const jobs = jobData?.jobs || [];
   const feed = feedData?.feed || [];
-
-  console.log("below is feed");
-  console.log(feed);
+  const profile = data?.me || {};
+ 
 
   if (!jobLoading) {
     console.log(jobs);
@@ -20,8 +20,10 @@ export default function UserDashboard() {
 
   return (
     <>
+    <h1 className="text-5xl font-bold mx-8 mt-4">Hello {profile.firstName}! Welcome to Your DashBoard!</h1>
+    <div className="divider m-6 px-4"></div>
       <div className="container grid grid-cols-6 grid-rows-8">
-        <div className="box col-span-2 rounded overflow-scroll max-h-64 m-4 bg-slate-700">
+        <div className="box col-span-2 rounded overflow-scroll max-h-96 m-4 bg-slate-700">
           <h1 className="text-4xl text-white m-2 text-center rounded bg-slate-400 ">
             Recent Job Postings
           </h1>
@@ -101,19 +103,25 @@ export default function UserDashboard() {
         </div>
         <div className="col-span-3 row-span-8 bg-slate-900 rounded h-min-44 m-6">
           <div className="m-4">
-          <PostForm />
+            <PostForm />
           </div>
           <div className="Feed-Containter grid grid-cols-1 bg-slate-500 overflow-scroll max-h-screen  rounded p-4">
             {feed.map((feed) => (
               <div className="Card  bg-slate-700 shadow-xl p-5 m-4 rounded">
                 <div className="avatar">
                   <div className="w-12 rounded-full">
-                    <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                    <img
+                      src={
+                        "/images/stock/photo-1534528741775-53994a69daeb.jpg" ||
+                        "https://png.pngtree.com/png-vector/20190221/ourlarge/pngtree-female-user-vector-avatar-icon-png-image_691506.jpg"
+                      }
+                    />
                   </div>
                   <h2 className="card-title text-center ml-5">Michael Mount</h2>
-                  
                 </div>
-                <p className="bg-black text-green-500 rounded p-5 my-2">{feed.postBody}</p>
+                <p className="bg-black text-green-500 rounded p-5 my-2">
+                  {feed.postBody}
+                </p>
               </div>
             ))}
           </div>
